3378ac34282eebc8caa3903aece4ae7267a06a9f
shxgg <andresxogo@gmail.com>
2023-02-08 17:11:52 -0300

    datos personales

--- JS/sintaxis.js
+++ JS/sintaxis.js
@@ -4,4 +4,11 @@ document.getElementById('boton').onclick = function () {
 
     document.getElementById("printCV").innerHTML = "gracias por su interes!"
 
-}
+};
+
+document.getElementById('referencias').onclick = function () {
+
+    document.getElementById("botonr").innerHTML = "gracias por su interes!"
+
+};
+
